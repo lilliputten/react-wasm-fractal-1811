96dc9ef67413d0cc612974b29d4c1ef67c82d51c
lilliputten <lilliputten@yandex.ru>
2018-11-24 19:30:31 +0300

    initial config

--- src/canvas.js
+++ src/canvas.js
@@ -1,26 +1,26 @@
-import React, {Component} from "react";
+import React, {Component} from 'react';
 
-const wasm = import("./fractal.wasm");
+const wasm = require('./fractal.wasm');
 
 class Canvas extends Component {
 
   componentDidMount() {
-      wasm.then(wasm => {
-    const mandelIterWASM = wasm._Z10mandelIterffi;
-    let canvas = this.refs.canvas.getContext('2d');
-    let mag = 200;
-    let panX = 2;
-    let panY = 1.25;
-    let maxIter = 100;
+    wasm.then(wasm => {
+      const mandelIterWASM = wasm._Z10mandelIterffi;
+      let canvas = this.refs.canvas.getContext('2d');
+      let mag = 200;
+      let panX = 2;
+      let panY = 1.25;
+      let maxIter = 100;
 
-    for (let x = 10; x < this.props.height; x++)  {
-      for (let y = 10; y < this.props.width; y++)  {
-        let m = mandelIterWASM(x/mag - panX, y/mag - panY, maxIter);
-        canvas.fillStyle = (m === 0) ? '#000' : 'hsl(0, 100%, ' + m + '%)';
-        canvas.fillRect(x, y, 1,1);
+      for (let x = 10; x < this.props.height; x++)  {
+        for (let y = 10; y < this.props.width; y++)  {
+          let m = mandelIterWASM(x/mag - panX, y/mag - panY, maxIter);
+          canvas.fillStyle = (m === 0) ? '#000' : 'hsl(0, 100%, ' + m + '%)';
+          canvas.fillRect(x, y, 1,1);
+        }
       }
-    }
-  });
+    });
   }
 
   render() {
